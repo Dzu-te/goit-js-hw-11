8563305873890f02a95c0c052844f521cde5e77d
Dzu-te <108475129+Dzu-te@users.noreply.github.com>
2024-01-29 15:04:52 +0200

    1

--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BASE_URL = `${window.location.protocol}//pixabay.com/api`;
+const BASE_URL = `${window.location.protocol}//pixabay.com/api/`;
 const API_KEY = '42024454-8ed2ac239bcd0125bd4fa3d9e';
 const fullUrl = `${BASE_URL}?key=${API_KEY}`;
 
 const form = document.querySelector('.searchPhotos');
 const cardContainer = document.querySelector('.card_container');
-
 
 const loader = document.createElement('div');
 loader.classList.add('loader');
@@ -25,7 +24,7 @@ function hideLoader() {
 form.addEventListener('submit', handleSubmit);
 
 function fetchImages(q) {
-  showLoader(); 
+  showLoader();
 
   const params = new URLSearchParams({
     key: API_KEY,
@@ -54,7 +53,8 @@ function handleSubmit(event) {
   if (!searchValue) {
     iziToast.warning({
       title: 'Warning',
-      message: 'Sorry, there are no images matching your search query. Please try again!',
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
     });
     return;
   }
@@ -68,12 +68,13 @@ function handleSubmit(event) {
       console.error('Error fetching images:', error);
       iziToast.error({
         title: 'Error',
-        message: 'An error occurred while fetching images. Please try again later.',
+        message:
+          'An error occurred while fetching images. Please try again later.',
       });
     })
     .finally(() => {
-      hideLoader(); 
-      searchInput.value = ''; 
+      hideLoader();
+      searchInput.value = '';
     });
 }
 
@@ -89,8 +90,35 @@ function createMarkup(images) {
     const img = document.createElement('img');
     img.src = image.webformatURL;
     img.alt = image.tags;
-    
+
+    const info = document.createElement('div');
+    info.classList.add('info');
+
+    const likes = document.createElement('span');
+    likes.classList.add('info-item');
+    likes.innerHTML = `<strong>Likes:</strong> <p class=${"pNumber"}>${image.likes}</p>`;
+
+    const views = document.createElement('span');
+    views.classList.add('info-item');
+    views.innerHTML = `<strong>Views:</strong> <p class=${"pNumber"}>${image.views}</p>`;
+
+    const comments = document.createElement('span');
+    comments.classList.add('info-item');
+    comments.innerHTML = `<strong>Comments:</strong> <p class=${"pNumber"}>${image.comments}</p>`;
+
+    const downloads = document.createElement('span');
+    downloads.classList.add('info-item');
+    downloads.innerHTML = `<strong>Downloads:</strong> <p class=${"pNumber"}>${image.downloads}</p>`;
+
+    info.appendChild(likes);
+    info.appendChild(views);
+    info.appendChild(comments);
+    info.appendChild(downloads);
+
     card.appendChild(img);
+
+    card.appendChild(info);
+
     cardContainer.appendChild(card);
   });
 }
